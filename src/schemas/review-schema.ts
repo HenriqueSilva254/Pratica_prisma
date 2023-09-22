4764a6d2cb47b9d0b774fb56fa5dbd66f23f684f
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-09-21 23:07:11 -0300

    prisma integrado

--- src/schemas/review-schema.ts
+++ src/schemas/review-schema.ts
@@ -2,10 +2,10 @@ import joi from "joi";
 
 import { CreateReview } from "../protocols/review";
 
-const REQUIRED_REVIEW_LENGTH = 100;
+const REQUIRED_REVIEW_LENGTH = 10;
 
 export const reviewSchema = joi.object<CreateReview>({
   bookId: joi.number().required(),
   grade: joi.number().min(0).max(10).required(),
   review: joi.string().min(REQUIRED_REVIEW_LENGTH).required()
-});
+}); 
